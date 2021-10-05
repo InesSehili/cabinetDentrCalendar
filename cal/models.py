91afbffbdac2021cdcf6d23aeaba318680e1c555
Ines Sehili <ines.sehili@univ-constantine2.dz>
2021-10-05 12:14:31 +0200

    modeifier levenement

--- cal/models.py
+++ cal/models.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
     @property
     def get_html_url(self):
         url = reverse('cal:event_edit', args=(self.id,))
-        return f'<a href="{url}"> {self.title} {self.start_time} </a>'
+        return f'<a href="{url}"> {self.title} </a>'
